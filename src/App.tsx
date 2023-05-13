11b6cf829d383714aee89d4c987abd38b08da906
ArtemStar2 <artemdegtyrov@gmail.com>
2023-05-13 19:14:54 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -31,31 +31,29 @@ const App = () => {
 
     return (
         <>
-        <BrowserRouter>
-                <Layout>
-                    <Routes>
-                        <Route path="/menu" element={<MenuList />}/>
-                        <Route path="/menu/:id" element={<MenuItem />}/>
+        <Layout>
+            <Routes>
+                <Route path="/menu" element={<MenuList />}/>
+                <Route path="/menu/:id" element={<MenuItem />}/>
 
-                        <Route path="/admin" element={!store.isAuth ? <LoginForm /> :<div>Панель</div>}/>
-                        <Route path="/admin/users" element={!store.isAuth ? <LoginForm /> : <Users />}/>
-                        <Route path="/admin/product" element={!store.isAuth ? <LoginForm /> : <Products />}/>
-                        <Route path="/admin/product/create" element={!store.isAuth ? <LoginForm /> : <ProductsCreate />}/>
-                        <Route path="/admin/product/:id" element={!store.isAuth ? <LoginForm /> : <ProductsUpdate />}/>
-                        <Route path="/admin/events/create" element={!store.isAuth ? <LoginForm /> : <EventCreate />}/>
-                        <Route path="/admin/events/" element={!store.isAuth ? <LoginForm /> : <Event />}/>
-                        <Route path="/admin/events/:id" element={!store.isAuth ? <LoginForm /> : <EventUpdate />}/>
-                        <Route path="/admin/tables/" element={!store.isAuth ? <LoginForm /> : <Tables />}/>
-                        <Route path="/admin/tables/create" element={!store.isAuth ? <LoginForm /> : <TableCreate />}/>  
-                        <Route path="/admin/tables/:id" element={!store.isAuth ? <LoginForm /> : <TableUpdate />}/>
-                        <Route path="/admin/contact/" element={!store.isAuth ? <LoginForm /> : <Contact />}/>
-                        <Route path="/admin/orders/create" element={!store.isAuth ? <LoginForm /> : <OrderCreate />}/>
-                        <Route path="/admin/orders/" element={!store.isAuth ? <LoginForm /> : <Order />}/>
-                        <Route path="/admin/orders/:id" element={!store.isAuth ? <LoginForm /> : <EventUpdate />}/>
-                        <Route path="*" element={<div>Error</div>}/>
-                    </Routes>
-                </Layout> 
-        </BrowserRouter>
+                <Route path="/admin" element={!store.isAuth ? <LoginForm /> :<div>Панель</div>}/>
+                <Route path="/admin/users" element={!store.isAuth ? <LoginForm /> : <Users />}/>
+                <Route path="/admin/product" element={!store.isAuth ? <LoginForm /> : <Products />}/>
+                <Route path="/admin/product/create" element={!store.isAuth ? <LoginForm /> : <ProductsCreate />}/>
+                <Route path="/admin/product/:id" element={!store.isAuth ? <LoginForm /> : <ProductsUpdate />}/>
+                <Route path="/admin/events/create" element={!store.isAuth ? <LoginForm /> : <EventCreate />}/>
+                <Route path="/admin/events/" element={!store.isAuth ? <LoginForm /> : <Event />}/>
+                <Route path="/admin/events/:id" element={!store.isAuth ? <LoginForm /> : <EventUpdate />}/>
+                <Route path="/admin/tables/" element={!store.isAuth ? <LoginForm /> : <Tables />}/>
+                <Route path="/admin/tables/create" element={!store.isAuth ? <LoginForm /> : <TableCreate />}/>  
+                <Route path="/admin/tables/:id" element={!store.isAuth ? <LoginForm /> : <TableUpdate />}/>
+                <Route path="/admin/contact/" element={!store.isAuth ? <LoginForm /> : <Contact />}/>
+                <Route path="/admin/orders/create" element={!store.isAuth ? <LoginForm /> : <OrderCreate />}/>
+                <Route path="/admin/orders/" element={!store.isAuth ? <LoginForm /> : <Order />}/>
+                <Route path="/admin/orders/:id" element={!store.isAuth ? <LoginForm /> : <EventUpdate />}/>
+                <Route path="*" element={<div>Error</div>}/>
+            </Routes>
+        </Layout> 
         <Toaster />
         </>
     )
