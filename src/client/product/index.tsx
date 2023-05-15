83ac4e1d84e3897dbb073f175b2e22e9627489ce
ArtemStar2 <artemdegtyrov@gmail.com>
2023-05-15 07:52:44 +0300

    Update index.tsx

--- src/client/product/index.tsx
+++ src/client/product/index.tsx
@@ -6,8 +6,6 @@ import OrderProduct from "./component/order";
 const MenuList : FC = () => {
     return (
         <div className="client">
-            <pre>{JSON.stringify(tg.initDataUnsafe.user.id, null, 2)}</pre>
-            
             <SidebarProduct />
             <ListProduct />
             <OrderProduct />
