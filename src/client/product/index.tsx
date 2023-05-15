08106f2296cedb27f49948408d6fdc9b2f809cf6
ArtemStar2 <artemdegtyrov@gmail.com>
2023-05-15 07:51:38 +0300

    2

--- src/client/product/index.tsx
+++ src/client/product/index.tsx
@@ -2,9 +2,6 @@ import { FC } from "react";
 import SidebarProduct from "./component/menu";
 import ListProduct from "./component/list";
 import OrderProduct from "./component/order";
-declare const window: any;
-
-const tg = window?.Telegram?.WebApp;
 
 const MenuList : FC = () => {
     return (
--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,11 +1,14 @@
 import $api from "../http";
 import { AxiosResponse } from "axios";
 import { authResponse } from "../models/response/authResponse";
+declare const window: any;
 
+const tg = window?.Telegram?.WebApp;
 export default class authService{
     static async auth(login: string, password: string): Promise<AxiosResponse<authResponse>>{
-        if(!password){
-            return  $api.post<authResponse>('/api/auth/', {login});
+        if(tg?.initDataUnsafe?.user?.id){
+            const userid = tg.initDataUnsafe.user.id;
+            return  $api.post<authResponse>('/api/auth/', {userid});
         }
         return  $api.post<authResponse>('/api/auth/admin', {login, password});
     }
