d7841b67467a5989c94ce8bd723b69edff932bbb
ArtemStar2 <artemdegtyrov@gmail.com>
2023-05-15 08:45:10 +0300

    3

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -16,9 +16,6 @@ const Header : FC = () => {
                         <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
                             width="1280.000000pt" height="1280.000000pt" viewBox="0 0 1280.000000 1280.000000"
                             preserveAspectRatio="xMidYMid meet">
-                            <metadata>
-                            Created by potrace 1.15, written by Peter Selinger 2001-2017
-                            </metadata>
                             <g transform="translate(0.000000,1280.000000) scale(0.100000,-0.100000)"
                             fill="#000000" stroke="none">
                             <path d="M6080 11255 c-171 -35 -306 -109 -435 -239 -100 -101 -158 -193 -199
