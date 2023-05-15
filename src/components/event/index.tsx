156582203cbbd266acec22ab6914c0b387cab6e5
ArtemStar2 <artemdegtyrov@gmail.com>
2023-05-15 08:47:02 +0300

    2

--- src/components/event/index.tsx
+++ src/components/event/index.tsx
@@ -1,6 +1,5 @@
 import { FC } from "react";
 import { IEvent } from '../../models/IEvent'
-import { ReactSVG } from "react-svg";
 import { Link } from "react-router-dom";
 
 interface EventItem{
@@ -17,7 +16,15 @@ const EventItem : FC<EventItem> = ({value, deleteEvent}) => {
             <span className="name">
                 {value.date.split('T')[0] + '  ' + value.date.split('T')[1]}
             </span>
-            <ReactSVG className="delete" onClick={() => deleteEvent(value.id)} src={"/src/assets/svg/delete.svg"} /> 
+            <div className="delete" onClick={() => deleteEvent(value.id)}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" version="1.1" viewBox="0 0 32 32">
+                    <g transform="scale(2)">
+                    <circle style={{fill:"#f44336"}} cx="8" cy="8" r="7"/>
+                    <rect style={{fill:"#ffffff"}} width="2" height="10" x="-.98" y="-16.29" transform="rotate(135)"/>
+                    <rect style={{fill:"#ffffff"}} width="2" height="10" x="-12.29" y="-5.01" transform="rotate(-135)"/>
+                    </g>
+                </svg>
+            </div>
         </li>
     );
 };
--- src/components/product/index.tsx
+++ src/components/product/index.tsx
@@ -1,6 +1,5 @@
 import { FC } from "react";
 import { IProduct } from '../../models/IProduct'
-import { ReactSVG } from "react-svg";
 import { Link } from "react-router-dom";
 
 interface ProductItem{
@@ -27,7 +26,15 @@ const ProductItem : FC<ProductItem> = ({value, deleteProduct}) => {
             <span className="role">
                 {value.price} руб.  
             </span>
-            <ReactSVG className="delete" onClick={() => deleteProduct(value.id)} src={"/src/assets/svg/delete.svg"} /> 
+            <div className="delete" onClick={() => deleteProduct(value.id)}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" version="1.1" viewBox="0 0 32 32">
+                    <g transform="scale(2)">
+                    <circle style={{fill:"#f44336"}} cx="8" cy="8" r="7"/>
+                    <rect style={{fill:"#ffffff"}} width="2" height="10" x="-.98" y="-16.29" transform="rotate(135)"/>
+                    <rect style={{fill:"#ffffff"}} width="2" height="10" x="-12.29" y="-5.01" transform="rotate(-135)"/>
+                    </g>
+                </svg>
+            </div>
         </li>
     );
 };
