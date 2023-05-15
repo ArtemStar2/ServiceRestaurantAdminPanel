70fd59ba5f49947360e3e5404ccde04f5a9105e2
ArtemStar2 <artemdegtyrov@gmail.com>
2023-05-15 07:34:55 +0300

    2

--- src/client/product/index.tsx
+++ src/client/product/index.tsx
@@ -2,10 +2,14 @@ import { FC } from "react";
 import SidebarProduct from "./component/menu";
 import ListProduct from "./component/list";
 import OrderProduct from "./component/order";
+declare const window: any;
 
+const tg = window?.Telegram?.WebApp;
+console.log(tg);
 const MenuList : FC = () => {
     return (
         <div className="client">
+            {...tg}
             <SidebarProduct />
             <ListProduct />
             <OrderProduct />
--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,10 +1,7 @@
 import $api from "../http";
 import { AxiosResponse } from "axios";
 import { authResponse } from "../models/response/authResponse";
-declare const window: any;
 
-const tg = window?.Telegram?.WebApp;
-console.log(tg);
 export default class authService{
     static async auth(login: string, password: string): Promise<AxiosResponse<authResponse>>{
         if(!password){
